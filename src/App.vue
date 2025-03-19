5793a209afbd0cf6b0ba403b3ae7165c43241108
Filipe <pro.maiscommentz@gmail.com>
2025-03-19 10:30:36 +0100

    ✨ Add custom cursor with GSAP

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
 <template>
   <div class="flex flex-col min-h-screen">
+    <!-- Custom cursor -->
+    <div class="cursor hidden md:flex">
+      <div class="cursor__ball cursor__ball--big fill-[#e0e0e0]">
+        <svg width="80" height="80" viewBox="0 0 30 30">
+          <circle cx="15" cy="15" r="12" />
+        </svg>
+      </div>
+      <div class="cursor__ball cursor__ball--small fill-cWhite">
+        <svg width="10" height="10" viewBox="0 0 10 10">
+          <circle cx="5" cy="5" r="4" />
+        </svg>
+      </div>
+    </div>
+
     <!-- Header -->
-     <Header/>
+    <Header/>
     <!-- Main Content Section -->
     <main class="container mx-auto flex-1 p-6">
       <RouterView />
@@ -12,8 +26,25 @@
 </template>
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import { gsap } from 'gsap'
 import Header from './components/Header.vue'
 import Footer from './components/Footer.vue'
+
+const bigBall = ".cursor__ball--big";
+const smallBall = ".cursor__ball--small"
+document.body.addEventListener('mousemove', onMouseMove);
+
+// Move the cursor
+function onMouseMove(e) {
+  gsap.to(bigBall, .5, {
+    x: e.pageX - 40,
+    y: e.pageY - window.scrollY - 40
+  })
+  gsap.to(smallBall, .1, {
+    x: e.pageX - 5,
+    y: e.pageY - window.scrollY - 5
+  })
+}
 </script>
 <style scoped>
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,11 @@ export default {
       },
     },
   },
+  variants: {
+    extend: {
+      mixBlendMode: ['hover', 'focus'],
+    },
+  },
   plugins: [],
 }
 

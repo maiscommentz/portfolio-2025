63b80539cf283cb7d8b21c67298089f3c8c6893a
Filipe <pro.maiscommentz@gmail.com>
2025-03-19 11:15:19 +0100

    ✨ Add Button component with primary and secondary styles (hover not implemented yet)

---
+++ src/components/Button.vue
@@ -0,0 +1,14 @@
+<template>
+    <button v-if="type == 'primary'" class="bg-cBlack text-white px-4 py-2 rounded-md">
+        <slot></slot>
+    </button>
+    <button v-else-if="type == 'secondary'" class="bg-cWhite text-cBlack outline-2 outline-cBlack outline px-4 py-2 rounded-md">
+        <slot></slot>
+    </button>
+</template>
+<script setup lang="ts">
+defineProps({
+    type: String,
+    
+})
+</script>

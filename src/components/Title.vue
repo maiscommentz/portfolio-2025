e610d6f540e5ced36336074ea124dcdf6410646d
Filipe <pro.maiscommentz@gmail.com>
2025-03-19 13:51:49 +0100

    ✨ Add Title component with customizable slots

---
+++ src/components/Title.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="flex flex-row w-full space-x-6 items-center">
+        <div class="bg-cGreen font-semibold text-3xl text-nowrap p-2">
+            <slot name="title"></slot>
+        </div>
+        <div class="text-sm text-wrap">
+            <slot name="subtitle"></slot>
+        </div>
+    </div>
+</template>

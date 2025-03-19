8dc3a30e3b2a6158a2f58127196b2d92734fa5d4
maiscommentz <pro.maiscommentz@gmail.com>
2025-03-20 00:10:21 +0100

    💄 Modify padding between sections

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
     <main>
         <!-- Section Hero -->
-        <div class="container mx-auto py-16 px-6">
+        <div class="container mx-auto py-12 px-6">
             <div class="flex flex-row">
                 <div class="w-full flex flex-col justify-center">
                     <h1 class="text-5xl font-semibold">{{ $t('hero.title') }}</h1>
@@ -12,24 +12,24 @@
                     </div>
                 </div>
                 <div class="w-full flex justify-center">
-                    <img src="/images/hero.png" alt="Illustration d'un développeur web" class="w-full" />
+                    <img src="/images/hero.svg" alt="Illustration d'un développeur web" class="w-full" />
                 </div>
             </div>
         </div>
         <!-- Down Arrow -->
-        <div class="flex justify-center py-8">
+        <div class="flex justify-center py-6">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 cursor-pointer text-cBlack animate-bounce" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
             </svg>
         </div>
         <!-- Section About -->
-        <div id="about" class="container mx-auto py-16 px-6">
+        <div id="about" class="container mx-auto py-24 px-6">
             <Title>
                 <template v-slot:title>{{ $t('about.title') }}</template>
                 <template v-slot:subtitle>{{ $t('about.subtitle') }}</template>
             </Title>
             <div class="flex flex-row justify-center space-x-16">
-                <img src="/images/about.jpg" alt="Photos de moi" class="w-1/3 object-cover" />
+                <img src="/images/about.jpg" alt="Photos de moi" class="w-1/3 object-cover rounded-lg" />
                 <div class="flex flex-col w-2/3 justify-between">
                     <p class="text-lg" v-html="$t('about.description', {br: '<br><br>', bold: '<b>', nobold: '</b>'})"/>
                     <img src="/images/signature.png" alt="Signature de Filipe" class="w-1/2 mt-4" />
@@ -37,8 +37,16 @@
             </div>
         </div>
         <!-- Section Technologies -->
-        <div class="container mx-auto py-16 px-6">
+        <div class="container mx-auto py-24 px-6">
             <Carousel />
+        </div>
+        <!-- Section Career -->
+        <div id="career" class="container mx-auto py-24 px-6">
+            <Title>
+                <template v-slot:title>{{ $t('career.title') }}</template>
+                <template v-slot:subtitle>{{ $t('career.subtitle') }}</template>
+            </Title>
+            <Timeline/>
         </div>
     </main>
 </template>
@@ -46,4 +54,5 @@
 import Button from '@/components/Button.vue'
 import Title from '@/components/Title.vue'
 import Carousel from '@/components/Carousel.vue'
+import Timeline from '@/components/Timeline.vue'
 </script>

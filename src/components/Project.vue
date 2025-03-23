06ce0af2b6eb90f2099472d3e7437deac35e44a4
maiscommentz <pro.maiscommentz@gmail.com>
2025-03-23 23:48:06 +0100

    🚧 Projects section layout

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="outline outline-2 outline-cBlack p-6 rounded-lg">
-        <div class="flex flex-col-reverse xl:flex-row xl:space-x-4 h-full">
+    <div class="outline outline-2 outline-cBlack p-6 rounded-lg h-full w-full">
+        <div class="flex flex-col-reverse xl:flex-row xl:space-x-4 h-full w-full">
             <div class="w-full xl:w-1/2 flex flex-col justify-between mt-6">
                 <p class="text-lg" v-html="$t('projects.items.' + index + '.title', {bold: '<b>', nobold: '</b>'})"></p>
                 <div class="flex w-fit flex-row space-x-2 items-center hover:cursor-pointer mt-6">
@@ -8,7 +8,9 @@
                     <p class="text-lg">{{ $t('utils.seemore') }}</p>
                 </div>
             </div>
-            <img :src="'/images/projects/' + $t('projects.items.' + index + '.image')" :alt="'Projet ' + (index!+1)" class="w-full xl:w-1/2 outline outline-2 outline-cBlack object-cover rounded-lg" />
+            <div class="flex h-full w-full">
+                <img :src="'/images/projects/' + $t('projects.items.' + index + '.image')" :alt="'Projet ' + (index!+1)" class="outline outline-2 outline-cBlack object-cover rounded-lg" />
+            </div>
         </div>
     </div>
 </template>

3d3c634b1a4660d4e50ee533361bfefec41f0c0e
maiscommentz <pro.maiscommentz@gmail.com>
2025-03-23 20:08:17 +0100

    ✨ Add contact section to footer with email, LinkedIn, and GitHub links

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,27 @@
 <template>
-    <footer class="bg-gray-800 text-white py-6">
-        <div class="container mx-auto text-center">
-            <p>&copy; 2024 MySite. All Rights Reserved.</p>
-            <div class="flex justify-center space-x-6 mt-4">
-                <RouterLink to="" class="hover:text-gray-400">Privacy Policy</RouterLink>
-                <RouterLink to="" class="hover:text-gray-400">Terms of Service</RouterLink>
-                <RouterLink to="" class="hover:text-gray-400">Contact Us</RouterLink>
+    <footer>
+        <div id="contact" class="container mx-auto py-12 px-6">
+            <div class="flex flex-row items-center bg-cBlack text-white rounded-lg px-12 py-12">
+                <div class="w-2/3">
+                    <h2 class="text-4xl font-semibold">{{ $t('contact.title') }}</h2>
+                    <div class="mt-12 text-xl">
+                        <div class="flex items-center mb-6 cursor-pointer hover:underline underline-offset-4">
+                            <img src="/icons/email.svg" alt="Icone email" class="w-8 h-8 mr-4" />
+                            <a>{{ $t('contact.email') }}</a>
+                        </div>
+                        <div class="flex items-center mb-6 cursor-pointer hover:underline underline-offset-4">
+                            <img src="/icons/linkedin.svg" alt="Icone Linkedin" class="w-8 h-8 mr-4" />
+                            <a>{{ $t('contact.linkedin') }}</a>
+                        </div>
+                        <div class="flex items-center mb-6 cursor-pointer hover:underline underline-offset-4">
+                            <img src="/icons/github.svg" alt="Icone Github" class="w-8 h-8 mr-4" />
+                            <a>{{ $t('contact.github') }}</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-1/3 h-full flex justify-center">
+                    <img src="/images/contact.svg" alt="Illustration de contact" class="w-3/5" />
+                </div>
             </div>
         </div>
     </footer>

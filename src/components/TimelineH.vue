88487d4461d3d9c36cf117511ab642e5a996db45
Filipe Casimiro <pro.maiscommentz@gmail.com>
2025-03-27 23:27:57 +0100

    🐛 Fix label hover effects in TimelineH component

--- src/components/TimelineH.vue
+++ src/components/TimelineH.vue
@@ -18,9 +18,9 @@
 
     <!-- Labels (HTML) -->
     <div v-for="(point, i) in points" :key="'label' + i"
-        class="absolute label transition-transform duration-300 ease-in-out hover:scale-105 z-10"
+        class="absolute label hover:cursor-pointer z-10"
         :style="labelStyles[i]">
-        <div class="bg-cBlack text-cWhite p-3 rounded-lg clickable w-[300px]">
+        <div class="bg-cBlack text-cWhite p-3 rounded-lg clickable w-[300px] hover:scale-105 transition-transform duration-300 ease-in-out">
             <p>{{ point.title }}</p>
             <div class="flex w-full justify-between items-center mt-1">
                 <p class=" text-cGreen">{{ point.date }}</p>

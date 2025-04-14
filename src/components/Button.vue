c7d00b578c80d2fa620d98dab382b4cd0ad9d672
Filipe <pro.maiscommentz@gmail.com>
2025-04-14 15:08:03 +0200

    💄 Add hover effect to buttons

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,8 +1,8 @@
 <template>
-    <button v-if="type == 'primary'" class="bg-cBlack outline-2 outline-cBlack outline text-cWhite px-4 py-2 rounded-md">
+    <button v-if="type == 'primary'" class="bg-cBlack outline-2 outline-cBlack outline text-cWhite px-4 py-2 rounded-md hover:bg-cWhite hover:text-cBlack transition duration-300 ease-in-out">
         <slot></slot>
     </button>
-    <button v-else-if="type == 'secondary'" class="bg-cWhite text-cBlack outline-2 outline-cBlack outline px-4 py-2 rounded-md">
+    <button v-else-if="type == 'secondary'" class="bg-cWhite text-cBlack outline-2 outline-cBlack outline px-4 py-2 rounded-md hover:bg-cBlack hover:text-cWhite transition duration-300 ease-in-out">
         <slot></slot>
     </button>
 </template>

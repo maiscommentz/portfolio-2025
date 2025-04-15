bc9f9f9a317356020999c7cff24aceeb65230080
Filipe <pro.maiscommentz@gmail.com>
2025-04-15 16:42:01 +0200

    📱 Update responsiveness of project details page

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,7 +4,7 @@
             <div class="flex flex-col md:flex-row items-center bg-cBlack text-white rounded-lg px-12 py-12">
                 <div class="md:w-2/3">
                     <h2 class="text-3xl md:text-4xl font-semibold">{{ $t('contact.title') }}</h2>
-                    <div class="mt-12 md:text-xl">
+                    <div class="mt-12 md:text-xl break-all">
                         <div class="flex items-center mb-6 cursor-pointer hover:underline underline-offset-4 w-fit">
                             <img src="/icons/email.svg" alt="Icone email" class="w-8 h-8 mr-4" />
                             <a :href="`mailto:${$t('contact.email', {at: '@'})}`">{{ $t('contact.email', {at: '@'}) }}</a>
--- src/views/Project.vue
+++ src/views/Project.vue
@@ -1,16 +1,16 @@
 <template>
     <main>
-        <div class="container mx-auto md:h-full pb-12 px-6">
-            <div class="flex flex-col md:flex-row h-full bg-cBlack text-white rounded-lg px-12 py-12">
-                <div class="w-full md:w-1/2 md:flex justify-center lg:pr-12">
+        <div class="container mx-auto lg:h-full pt-12 md:pt-0 pb-12 px-6">
+            <div class="flex flex-col lg:flex-row h-full bg-cBlack text-white rounded-lg px-12 py-12">
+                <div class="w-full lg:w-1/2 lg:flex justify-center lg:pr-12 lg:pb-0 pb-12">
                     <img :src="'/images/projects/' + project.image" :alt="'Projet ' + project.id" class="flex h-full w-full outline outline-2 outline-cWhite object-cover rounded-lg" />
                 </div>
-                <div class="w-full md:w-1/2 flex flex-col justify-between">
+                <div class="w-full lg:w-1/2 flex flex-col justify-between">
                     <div>
-                        <h1 class="text-4xl text-center md:text-left" v-html="$t(`projects.items.${projectId}.title`, {bold: '<b>', nobold: '</b>'})"></h1>
+                        <h1 class="text-3xl md:text-4xl text-left" v-html="$t(`projects.items.${projectId}.title`, {bold: '<b>', nobold: '</b>'})"></h1>
                         <p class="text-lg text-cGreen pt-6" v-html="$t(`projects.items.${projectId}.date`, {br: '<br><br>', bold: '<b>', nobold: '</b>'})"></p>
                     </div>
-                    <div class="flex space-x-6">
+                    <div class="flex flex-col md:flex-row space-x-0 md:space-x-6 ">
                         <a v-if="project.demo" :href="project.demo_link">
                             <Button type="tertiary" active class="mt-6 w-full md:w-fit">
                                 {{ $t('utils.projects.demo') }}
